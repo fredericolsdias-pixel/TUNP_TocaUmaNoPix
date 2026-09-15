8c2c8fddb4a79adc4a200140f47d568819659458
fredericolsdias-pixel <frederico.lsdias@gmail.com>
2026-09-15 14:37:43 -0300

    backend formalizado

--- Backend/app/Http/Controllers/MusicaController.php
+++ Backend/app/Http/Controllers/MusicaController.php
@@ -50,3 +50,4 @@ class MusicaController extends Controller
     {
         
     }
+}
--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\MusicoController;
+use App\Http\Controllers\MusicaController;
+use App\Http\Controllers\ShowController;    
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('musicos',MusicoController::class);
+Route::apiResource('musicas',MusicaController::class);
+Route::apiResource('shows',ShowController::class);
